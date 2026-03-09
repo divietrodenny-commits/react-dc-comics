11428b2d887b2109c1965647b792f15505f84943
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-09 12:17:55 +0100

    Aggiunta img principale, titolo e bottone in appmain

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -3,9 +3,16 @@ export default function AppMain() {
 
     return (
         <main>
+            <section className="hero">
+                <img src="/img/jumbotron.jpg" alt="" />
+            </section>
+
+            <h2>CURRENT SERIES</h2>
+
             {comics.map((comic) => (
                 <div key={comic.id}>{comic.title}</div>
             ))}
+            <button className="load_more">LOAD MORE</button>
         </main>
     );
 }
