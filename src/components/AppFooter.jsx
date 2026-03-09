02a9d113e96f1c1486a710b07780084ca9421683
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-09 12:36:19 +0100

    iniziata bar in appfooter

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,6 +1,22 @@
 export default function AppFooter() {
+    const barBlue = [
+        { id: 1, icon: './img/buy-comics-digital-comics.png', label: 'Digital Comics'},
+        { id: 2, icon: './img/buy-comics-digital-comics.png', label: 'Digital Comics' },
+        { id: 3, icon: './img/buy-comics-digital-comics.png', label: 'Digital Comics' },
+        { id: 4, icon: './img/buy-comics-digital-comics.png', label: 'Digital Comics' },
+        { id: 5, icon: './img/buy-comics-digital-comics.png', label: 'Digital Comics' },
+    ];
 
     return (
-        <footer>Footer</footer>
-    )
+        <section className="bar_section">
+            <div className="bar_container">
+                {barBlue.map((bar) => (
+                    <div key={bar.id} className="strip_bar">
+                        <img src={bar.icon} alt={bar.label} />
+                        <span>{bar.label}</span>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
