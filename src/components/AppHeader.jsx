6744e20460437469dd53017dd37336cb53144b5c
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-09 11:32:11 +0100

    Aggiunto bootstrap

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,10 @@
 export default function AppHeader(){
-
     return(
-        <header>Header</header>
+        <header>
+            <img src="./img/dc-logo.png" alt="" />
+            <nav>
+
+            </nav>
+        </header>
     )
 }
